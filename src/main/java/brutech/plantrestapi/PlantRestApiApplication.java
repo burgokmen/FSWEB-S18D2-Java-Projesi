b835ceed7e9f145bf73002170cd62e930ddecd88
burgokmen <burgokmen@gmail.com>
2023-10-25 00:18:52 +0300

    init

---
+++ src/main/java/brutech/plantrestapi/PlantRestApiApplication.java
@@ -0,0 +1,13 @@
+package brutech.plantrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/brutech/plantrestapi/PlantRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package brutech.plantrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
