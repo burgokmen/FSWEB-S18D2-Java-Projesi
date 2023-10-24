aaa0556baaa155761a35f6025abc1a100c686079
burgokmen <burgokmen@gmail.com>
2023-10-25 01:38:24 +0300

    Fruit Service impl

---
+++ src/main/java/brutech/plantrestapi/services/FruitServiceImpl.java
@@ -0,0 +1,55 @@
+package brutech.plantrestapi.services;
+
+import brutech.plantrestapi.dao.FruitRepository;
+import brutech.plantrestapi.entity.Fruit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FruitServiceImpl implements FruitService{
+
+    private FruitRepository fruitRepository;
+    @Autowired
+    public FruitServiceImpl(FruitRepository fruitRepository) {
+        this.fruitRepository = fruitRepository;
+    }
+
+    @Override
+    public List<Fruit> getByPriceAsc() {
+        return fruitRepository.getByPriceAsc();
+    }
+
+    @Override
+    public Fruit getById(Long id) {
+        Optional<Fruit> result = fruitRepository.findById(id);
+        if (result.isPresent()) {
+            return result.get();
+        }
+        throw new RuntimeException("Did not find fruit id - " + id);
+    }
+
+    @Override
+    public List<Fruit> getByPriceDesc() {
+        return fruitRepository.getByPriceDesc();
+    }
+
+    @Override
+    public Fruit save(Fruit fruit) {
+        return fruitRepository.save(fruit);
+    }
+
+    @Override
+    public Fruit delete(Long id) {
+        Fruit fruit = getById(id);
+        fruitRepository.delete(fruit);
+        return fruit;
+    }
+
+    @Override
+    public List<Fruit> searchByName(String name) {
+        return fruitRepository.searchByName(name);
+    }
+}
